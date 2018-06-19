d2f5a75bf2457914ec0b3d1aceb7be8db416ff59
Nick Hamlin <Nick.Hamlin@makingwaves.com>
2018-06-19 16:57:04 -0500

    Created BasePage Template

---
+++ TravelBug.Web/Controllers/BasePageController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+using EPiServer.Core;
+using EPiServer.Web.Mvc;
+using TravelBug.Model.Pages;
+
+namespace TravelBug.Controllers
+{
+	public abstract class BasePageController<TPageType> : PageController<TPageType> where TPageType : BasePage
+	{
+		
+	}
+}
--- TravelBug.Web/Controllers/StartPageController.cs
+++ TravelBug.Web/Controllers/StartPageController.cs
@@ -4,14 +4,14 @@ using TravelBug.Model.Pages;
 
 namespace TravelBug.Controllers
 {
-	public class StartPageController : PageController<StartPage>
+	public class StartPageController : BasePageController<StartPage>
 	{
-		public ActionResult Index(StartPage currentPage)
+		public ActionResult Index(StartPage currentContent)
 		{
 			/* Implementation of action. You can create your own view model class that you pass to the view or
              * you can pass the page type for simpler templates */
 
-			return View(currentPage);
+			return View(currentContent);
 		}
 	}
 }
