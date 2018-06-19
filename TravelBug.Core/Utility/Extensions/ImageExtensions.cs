7455e9331fc007f77ea07d34b972ecde992944c0
Nick Hamlin <Nick.Hamlin@makingwaves.com>
2018-06-19 16:48:01 -0500

    Added Image View

---
+++ TravelBug.Core/Utility/Extensions/ImageExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelBug.Core.Utility.Extensions
+{
+	public static class ImageExtensions
+	{
+		
+	}
+}
