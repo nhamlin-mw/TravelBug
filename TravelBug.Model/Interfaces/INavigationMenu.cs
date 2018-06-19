dcc70c5d8b7f303f1851f6ee2216323b5d0d5d2e
Nick Hamlin <Nick.Hamlin@makingwaves.com>
2018-06-19 16:53:19 -0500

    Added INavigationMenu interface

---
+++ TravelBug.Model/Interfaces/INavigationMenu.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EPiServer.Core;
+
+namespace TravelBug.Model.Interfaces
+{
+	public interface INavigationMenu : IContent
+	{
+		bool IsVisible { get; set; }
+	}
+}
--- TravelBug.Model/Pages/ContainerPage.cs
+++ TravelBug.Model/Pages/ContainerPage.cs
@@ -1,10 +1,14 @@
-﻿using EPiServer.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
+using EPiServer.DataAnnotations;
+using TravelBug.Model.Interfaces;
 
 namespace TravelBug.Model.Pages
 {
 	[ContentType(DisplayName = "Container Page", GUID = "7fc18fae-33c1-4f64-94c6-c5a279e6bf7e", Description = "")]
-	[ImageUrl("~/Content/images/icons/folder-icon.svg")]
-	public class ContainerPage : BaseContainer
+	[ImageUrl("~/Assets/images/icons/folder-icon.svg")]
+	public class ContainerPage : BaseContainer, INavigationMenu
 	{
+		[Display(Name = "Show in Navigation", Order = 10)]
+		public virtual bool IsVisible { get; set; }
 	}
 }
