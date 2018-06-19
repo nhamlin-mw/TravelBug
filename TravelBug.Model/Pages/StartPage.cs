2afc2810f5a8e09d8bd232b64fa962aafc549ae1
Nick Hamlin <Nick.Hamlin@makingwaves.com>
2018-06-19 16:57:16 -0500

    Housekeeping

--- TravelBug.Model/Pages/StartPage.cs
+++ TravelBug.Model/Pages/StartPage.cs
@@ -1,18 +1,26 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Razor.Parser.SyntaxTree;
 using EPiServer.Core;
 using EPiServer.DataAnnotations;
+using EPiServer.Web;
+using TravelBug.Model.Blocks;
 
 namespace TravelBug.Model.Pages
 {
-	[ContentType(DisplayName = "StartPage", GUID = "fb200427-d190-49a3-9200-cc120f3d22bb", Description = "")]
-	public class StartPage : PageData
+	[ContentType(DisplayName = "Start Page", GUID = "fb200427-d190-49a3-9200-cc120f3d22bb", Description = "")]
+	[ImageUrl("~/Assets/images/icons/start_page.png")]
+	public class StartPage : BasePage
 	{
-		[CultureSpecific]
-		[Display(Name = "Main Content Area", Order = 20)]
-		public virtual ContentArea MainContentArea { get; set; }
-		
 		[CultureSpecific]
 		[Display(Name = "Navigation Area", Order = 10)]
 		public virtual ContentArea NavContentArea { get; set; }
+		
+		[Display(Name = "Hero Block", Order = 20)]
+		[UIHint(UIHint.Block)]
+		public virtual HeroBlock HeroBlock { get; set; }
+
+		[CultureSpecific]
+		[Display(Name = "Main Content Area", Order = 30)]
+		public virtual ContentArea MainContentArea { get; set; }
 	}
 }
