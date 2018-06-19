a37da26106108a1853490a4dd2316e915f93aefd
Nick Hamlin <Nick.Hamlin@makingwaves.com>
2018-06-19 16:52:09 -0500

    Added folders for MVC to look for Views

---
+++ TravelBug.Web/Application/Configuration/TravelViewEngine.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+
+namespace TravelBug.Application.Configuration
+{
+	public class TravelViewEngine : RazorViewEngine
+	{
+		private static readonly string[] _additionalViewsFormats =
+		{
+			"~/Views/Blocks/{0}.cshtml",
+			"~/Views/Pages/{1}/{0}.cshtml",
+			"~/Views/Pages/{0}.cshtml",
+			"~/Views/Shared/PagePartials/{0}.cshtml",
+			"~/Views/Shared/Partials/{0}.cshtml",
+			"~/Views/Shared/Blocks/{0}.cshtml"
+		};
+
+		public TravelViewEngine()
+		{
+			PartialViewLocationFormats = PartialViewLocationFormats.Union(_additionalViewsFormats).ToArray();
+			ViewLocationFormats = ViewLocationFormats.Union(_additionalViewsFormats).ToArray();
+		}
+	}
+}
--- TravelBug.Web/Global.asax.cs
+++ TravelBug.Web/Global.asax.cs
@@ -1,14 +1,16 @@
 ﻿using System.Web.Mvc;
+using EPiServer;
+using TravelBug.Application.Configuration;
 
 namespace TravelBug
 {
-    public class EPiServerApplication : EPiServer.Global
-    {
-        protected void Application_Start()
-        {
-            AreaRegistration.RegisterAllAreas();
-
-            //Tip: Want to call the EPiServer API on startup? Add an initialization module instead (Add -> New Item.. -> EPiServer -> Initialization Module)
-        }
-    }
+	public class EPiServerApplication : Global
+	{
+		protected void Application_Start()
+		{
+			AreaRegistration.RegisterAllAreas();
+			ViewEngines.Engines.Add(new TravelViewEngine());
+			//Tip: Want to call the EPiServer API on startup? Add an initialization module instead (Add -> New Item.. -> EPiServer -> Initialization Module)
+		}
+	}
 }
