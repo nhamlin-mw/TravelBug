66e9324f7a7ab27740628b773f16306b01233b3b
Nick Hamlin <Nick.Hamlin@makingwaves.com>
2018-07-16 13:45:05 -0500

    Added Authentication

---
+++ TravelBug.Core/Utility/Nansen/Encore/ConfigurationUtility.cs
@@ -0,0 +1,41 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: Encore.Utilities.ConfigurationUtility
+// Assembly: Nansen.Encore.Core, Version=11.2.5.0, Culture=neutral, PublicKeyToken=null
+// MVID: CC483883-96C0-4B49-8B91-FF7427867AC7
+// Assembly location: C:\sourcecode\advanced-energy\packages\Nansen.Encore.Core.11.2.5\lib\net461\Nansen.Encore.Core.dll
+
+using System;
+using System.Configuration;
+
+namespace TravelBug.Core.Utility.Nansen.Encore
+{
+	/// <summary>Utilities related to configuration files</summary>
+	public static class ConfigurationUtility
+	{
+		/// <summary>
+		/// Returns a boolean appsetting, can be used when the service locator is not yet initialized
+		/// </summary>
+		internal static bool GetBoolAppsetting(string key, bool defaultValue)
+		{
+			bool result;
+			if (!bool.TryParse(ConfigurationManager.AppSettings[key], out result))
+				return defaultValue;
+			return result;
+		}
+
+		/// <summary>
+		/// Tries to read an appsetting strongly typed by using <see cref="M:System.Convert.ChangeType(System.Object,System.TypeCode)" /> to convert the string value.
+		/// </summary>
+		/// <typeparam name="T">Target type</typeparam>
+		/// <param name="key">The appsetting key</param>
+		/// <param name="fallback">Fallback value to return if the value is not found, or can not be converted to the target type if <paramref name="throwIfInvalid" /> is false.</param>
+		/// <param name="throwIfInvalid">If set true, the function will throw if conversion fails. It will *not* throw if the config value is null (not found).</param>
+		public static T GetAppSetting<T>(string key, T fallback, bool throwIfInvalid = false)
+		{
+			string appSetting = ConfigurationManager.AppSettings[key];
+			if (appSetting == null)
+				return fallback;
+			return (T)Convert.ChangeType(appSetting, typeof(T));
+		}
+	}
+}
---
+++ TravelBug.Web/Application/Security/StartupAuthenticationConfiguration.cs
@@ -0,0 +1,44 @@
+﻿#region header
+
+// Copyright (c) 2018
+// Author:         Nicholas Hamlin
+// Created Date:  07/10/2018
+// Filename: TravelBug:TravelBug.Web:AuthenticationConfiguration.cs
+// Usage:
+
+#endregion header
+
+using System;
+using EPiServer.Cms.UI.AspNetIdentity;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using TravelBug.Application.Security;
+using TravelBug.Core.Utility.Nansen.Encore;
+
+[assembly:OwinStartup(typeof(StartupAuthenticationConfiguration))]
+
+namespace TravelBug.Application.Security
+{
+	public class StartupAuthenticationConfiguration
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			// Add CMS integration for ASP.NET Identify
+			app.AddCmsAspNetIdentity<ApplicationUser>();
+			// Use cookie authentication
+			app.UseCookieAuthentication(new CookieAuthenticationOptions
+			{
+				AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+				LoginPath = new PathString(ConfigurationUtility.GetAppSetting("LoginUrl", "/Util/login.aspx").Trim('~')),
+				Provider = new CookieAuthenticationProvider
+				{
+					OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager<ApplicationUser>, ApplicationUser>(
+						validateInterval: TimeSpan.FromMinutes(30), regenerateIdentity: (manager, user) => manager.GenerateUserIdentityAsync(user))
+				}
+			});
+		}
+	}
+}
